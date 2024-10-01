2b047c3c5f410bfc08eabc5f48a84c70e8d99639
Tony-Elia <antony.safaga@gmail.com>
2024-10-01 15:19:51 -0400

    Display Activity w Scrolling Behavior

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -1,8 +1,10 @@
 package edu.temple.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class DisplayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,9 +13,8 @@ class DisplayActivity : AppCompatActivity() {
 
         // TODO Step 3: Extract transferred value and use for lyricsDisplayView text size
         with (findViewById<TextView>(R.id.lyricsDisplayTextView)) {
-
+            movementMethod = ScrollingMovementMethod()
+            textSize = intent.getIntExtra("textSize", 22).toFloat()
         }
-
-
     }
 }
